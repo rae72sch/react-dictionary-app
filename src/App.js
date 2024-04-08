1afa2e7bf82e6ebd95220aa36f3601392219317e
Rachel Schofield <rae72sch@hotmail.com>
2024-04-08 15:52:51 +0100

    Initial files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import dictionary from "./dictionary.svg"
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={dictionary} className="headerLogo" alt="logo" />
+      </header>
+    </div>
+  );
+}
