512b5acabf7af3c7117a2de601adfd8f36023eb7
Rachel Schofield <rae72sch@hotmail.com>
2024-04-09 14:37:31 +0100

    Integrating the basic API connection

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
+
 import "./Search.css";
 
 export default function Search() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    // documentation at: https://dictionaryapi.dev
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
