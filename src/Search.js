102a9eca612ff6c722cf2a7440cd89a54f0efa2b
Rachel Schofield <rae72sch@hotmail.com>
2024-04-29 07:56:19 +0100

    Added photos section

--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 import "./Search.css";
 
 export default function Search() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
   }
 
@@ -17,8 +19,16 @@ export default function Search() {
 
     // documentation at: https://dictionaryapi.dev
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleDictionaryResponse);
 
-    axios.get(apiUrl).then(handleResponse);
+    let pexelsApiKey = "cDDq0G42Ihr6zKNlPHFwV72X3iJ3IYsNZp2iCVSbUL6WIeaJMY1G4qZ2";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function handleKeywordChange(event) {
@@ -38,6 +48,7 @@ export default function Search() {
         </div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
